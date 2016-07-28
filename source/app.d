3e9b49bff406e23e6267fb7691221adfdc113c9f
Eric Scrivner <zenogais@gmail.com>
2016-07-27 21:01:45 -0700

    Fix: More Build Fixes

---
+++ source/app.d
@@ -0,0 +1,57 @@
+/**
+ * Application entry point for Phlogiston command-line tool.
+ *
+ * Copyright © 2016, Eric Scrivner
+ *
+ * License: Subject to the terms of the MIT license, as written in the included
+ * LICENSE.txt file.
+ * Authors: Eric Scrivner
+ */
+module phlogiston.phlogiston;
+
+import std.file;
+import std.stdio;
+
+import phlogiston.cli.assembler;
+import phlogiston.cli.disassembler;
+
+version(PhlogistonCli) {
+    int main(string[] args) {
+        if (args.length < 4) {
+            writeln("Usage: phlogiston [assemble|disassemble] [INPUT] [OUTPUT]");
+            writeln("The assembler converts an EVM assembly language file into a");
+            writeln("file containing a bytecode string. The disassembler converts");
+            writeln("an EVM bytecode file into an assembly language file.");
+            return 1;
+        }
+
+        auto mode = args[1];
+        if (mode != "assemble" && mode != "disassemble") {
+            writefln("error: unknown mode '%s'", mode);
+            writeln("valid modes are 'assemble' and 'disassemble'");
+            return 1;
+        }
+
+        auto inputFile = args[2];
+        if (!inputFile.exists) {
+            writefln("error: could not find INPUTFILE '%s'", inputFile);
+            return 1;
+        }
+
+        auto outputFile = args[3];
+        if (outputFile.exists) {
+            writefln("error: OUTPUTFILE '%s' already exists", outputFile);
+            return 1;
+        }
+
+        if (mode == "assemble") {
+            auto assemblerApp = new AssemblerApp();
+            assemblerApp.execute(inputFile, outputFile);
+        } else {
+            auto disassemblerApp = new DisassemblerApp();
+            disassemblerApp.execute(inputFile, outputFile);
+        }
+
+        return 0;
+    }
+}
